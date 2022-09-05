7d98e32098164d0d7c4bd8a821e4f73ad10da456
Bildeniz <Bildeniz56@gmail.com>
2022-09-05 20:40:40 +0300

    added progress bar

--- bussinnes.py
+++ bussinnes.py
@@ -1,15 +1,17 @@
 import string
 from pytube import YouTube
+from pytube.exceptions import RegexMatchError
+from pytube.cli import on_progress
 from hurry.filesize import size, alternative
-from tkinter import ttk, filedialog, END
+from tkinter import ttk, filedialog, END, messagebox, Tk
 
 class youtube:
     @staticmethod
-    def get_streams(url) -> dict:
+    def get_streams(url, pb) -> dict:
         """
         send a youtube url
         """
-        streams = YouTube(url).streams
+        streams = YouTube(url, on_progress_callback=lambda stream, chunk, bytes_remaining,: youtube.pb_func(stream, chunk, bytes_remaining, pb)).streams
         streams_dict = {}
         for stream in streams:
             if stream:
@@ -20,28 +22,40 @@ class youtube:
     def pretty_stream(stream):
         dict_stream = stream.__dict__
         if dict_stream['type'] == 'video':
-            return f"type:{dict_stream['type']}, subtype{dict_stream['subtype']}, fps:{stream.fps}, {size(dict_stream['_filesize'], system=alternative)}"
+            return f"type:{dict_stream['type']}, subtype:{dict_stream['subtype']}, res:{dict_stream['resolution']}, fps:{stream.fps}, {size(dict_stream['_filesize'], system=alternative)}"
         else:
-            return f"type:{dict_stream['type']}, res:{dict_stream['resolution']}, subtype:{dict_stream['subtype']}, {size(dict_stream['_filesize'], system=alternative)}"
+            return f"type:{dict_stream['type']}, subtype:{dict_stream['subtype']}, {size(dict_stream['_filesize'], system=alternative)}"
+
+    @staticmethod
+    def pb_func(stream, chunk, bytes_remaining, pb):
+        pb['value'] = youtube.percent(bytes_remaining, stream.filesize)
+
+    @staticmethod
+    def percent(tem, total):
+        try:
+            return str(100 - ((float(tem) / float(total)) * float(100)))[:5]
+        except ZeroDivisionError:
+            return 100
 
 class gui_command:
     @staticmethod
-    def check_button(url:string, combobox:ttk.Combobox):
-        stream_dict = youtube.get_streams(url)
+    def check_button(stream_dict, combobox:ttk.Combobox, root:Tk):
         combobox['values'] = list(stream_dict.keys())
+        root.config(cursor='')
+        root.grab_release()
 
     @staticmethod
     def locate_file(download_location_entry):
         file = filedialog.askdirectory()
         if file:
-            print(file)
             download_location_entry['state'] = 'normal'
             download_location_entry.delete(0, END)
             download_location_entry.insert(0, file)
             download_location_entry['state'] = 'disabled'
 
     @staticmethod
-    def download(youtube_url_entry, selectable_stream, download_location_entry):
-        stream_dict = youtube.get_streams(youtube_url_entry.get())
-        print(selectable_stream['values'][selectable_stream.current()])
+    def download(stream_dict, selectable_stream, download_location_entry, root):
+        root.config(cursor='wait')
         stream_dict[selectable_stream['values'][selectable_stream.current()]].download(output_path=download_location_entry.get())
+        messagebox.showinfo('Youtube Downloader', 'Download is successfuly')
+        root.config(cursor='')
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 from bussinnes import youtube, gui_command
+from pytube.exceptions import RegexMatchError
+import threading
+
+stream_dict = {}
+
+def check_button(combobox:ttk.Combobox, root:tk.Tk, pb):
+    global stream_dict
+    try:
+        root.config(cursor='wait')
+        stream_dict = youtube.get_streams(youtube_url_entry.get(), pb)
+        gui_command.check_button(stream_dict, combobox, root)
+    except RegexMatchError:
+        messagebox.showerror('Youtube Downloader', 'Please enter a Youtube URL')
+        root.config(cursor='')
+        exit()
+
 
 root = tk.Tk()
 root.title('Youtube Downloader')
@@ -14,22 +30,31 @@ youtube_url_entry = tk.Entry(youtube_entrys_frame, font=('Calibri', '16'))
 youtube_url_entry.insert(0, 'Youtube URL')
 youtube_url_entry.grid(column=0, row=0, padx=15, pady=12, sticky=tk.W)
 
-youtube_url_check_button = tk.Button(youtube_entrys_frame, height=1, text='Check', font=('Calibri'), borderwidth=0, command= lambda: gui_command.check_button(youtube_url_entry.get(), selectable_stream))
+youtube_url_check_button = tk.Button(youtube_entrys_frame, height=1, text='Check', font=('Calibri'), borderwidth=0, command=lambda: threading.Thread(
+    target=check_button, args=(selectable_stream, root, pb)).start())
 youtube_url_check_button.grid(row=0, column=1)
 
-selectable_stream = ttk.Combobox(root, font=('Calibri', '17'), width=23, state='readonly')
-selectable_stream.grid(column=0, row=1, padx=(25,0), pady=0)
+selectable_stream = ttk.Combobox(root, font=(
+    'Calibri', '17'), width=23, state='readonly')
+selectable_stream.grid(column=0, row=1, padx=(25, 0), pady=0)
 
 download_location_frame = tk.Frame(root, background='gray')
 download_location_frame.grid(column=0, row=2)
 
-download_location_entry = tk.Entry(download_location_frame, font=('Calibri', '16'), state='disabled')
-download_location_entry.grid(column=0, row=0, padx=(25,15), pady=12)
+download_location_entry = tk.Entry(
+    download_location_frame, font=('Calibri', '16'), state='disabled')
+download_location_entry.grid(column=0, row=0, padx=(25, 15), pady=12)
 
-download_location_button = tk.Button(download_location_frame, height=1, text='Browse', font=('Calibri'), borderwidth=0, command= lambda: gui_command.locate_file(download_location_entry))
+download_location_button = tk.Button(download_location_frame, height=1, text='Browse', font=(
+    'Calibri'), borderwidth=0, command=lambda: gui_command.locate_file(download_location_entry))
 download_location_button.grid(row=0, column=1)
 
-download_button = tk.Button(root, height=1, width=30 ,text='DOWNLOAD', font=('Calibri'), borderwidth=0, command=lambda: gui_command.download(youtube_url_entry, selectable_stream, download_location_entry))
-download_button.grid(row=3, column=0, sticky=tk.W, padx=27)
+pb = ttk.Progressbar(root, orient=tk.HORIZONTAL,
+                     length=250, mode='determinate')
+pb.grid(row=3, column=0, sticky=tk.W, padx=27, pady=(0, 20))
+
+download_button = tk.Button(root, height=1, width=30, text='DOWNLOAD', font=('Calibri'), borderwidth=0,
+                            command=lambda: threading.Thread(target=gui_command.download, args=(stream_dict, selectable_stream, download_location_entry, root)).start())
+download_button.grid(row=4, column=0, sticky=tk.W, padx=27)
 
 root.mainloop()
