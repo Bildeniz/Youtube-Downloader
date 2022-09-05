18b12291dde71460d1c130a56c7aa23711fe8cbb
Bildeniz <Bildeniz56@gmail.com>
2022-09-05 16:32:02 +0300

    update

--- bussinnes.py
+++ bussinnes.py
@@ -1,22 +1,47 @@
+import string
 from pytube import YouTube
 from hurry.filesize import size, alternative
-import math
+from tkinter import ttk, filedialog, END
 
 class youtube:
     @staticmethod
-    def get_streams(url):
+    def get_streams(url) -> dict:
+        """
+        send a youtube url
+        """
         streams = YouTube(url).streams
         streams_dict = {}
-        print(streams)
         for stream in streams:
             if stream:
-                # print(stream.__dict__)
-                streams_dict[stream] = youtube.pretty_stream(stream)
-                print(streams_dict)
+                streams_dict[youtube.pretty_stream(stream)] = stream
+        return streams_dict
 
     @staticmethod
     def pretty_stream(stream):
         dict_stream = stream.__dict__
-        return f"type:{dict_stream['type']}, res:{dict_stream['resolution']}, {size(dict_stream['_filesize'], system=alternative)}"
+        if dict_stream['type'] == 'video':
+            return f"type:{dict_stream['type']}, subtype{dict_stream['subtype']}, fps:{stream.fps}, {size(dict_stream['_filesize'], system=alternative)}"
+        else:
+            return f"type:{dict_stream['type']}, res:{dict_stream['resolution']}, subtype:{dict_stream['subtype']}, {size(dict_stream['_filesize'], system=alternative)}"
 
-youtube.get_streams('https://www.youtube.com/watch?v=rFUXfIO_bNk')
+class gui_command:
+    @staticmethod
+    def check_button(url:string, combobox:ttk.Combobox):
+        stream_dict = youtube.get_streams(url)
+        combobox['values'] = list(stream_dict.keys())
+
+    @staticmethod
+    def locate_file(download_location_entry):
+        file = filedialog.askdirectory()
+        if file:
+            print(file)
+            download_location_entry['state'] = 'normal'
+            download_location_entry.delete(0, END)
+            download_location_entry.insert(0, file)
+            download_location_entry['state'] = 'disabled'
+
+    @staticmethod
+    def download(youtube_url_entry, selectable_stream, download_location_entry):
+        stream_dict = youtube.get_streams(youtube_url_entry.get())
+        print(selectable_stream['values'][selectable_stream.current()])
+        stream_dict[selectable_stream['values'][selectable_stream.current()]].download(output_path=download_location_entry.get())
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from bussinnes import youtube
+from tkinter import ttk
+from bussinnes import youtube, gui_command
 
 root = tk.Tk()
 root.title('Youtube Downloader')
@@ -9,10 +10,26 @@ root.config(background='gray')
 youtube_entrys_frame = tk.Frame(root, background='gray')
 youtube_entrys_frame.grid(column=0, row=0)
 
-youtube_url_entry = tk.Entry(youtube_entrys_frame, font=('Calibri', '15'))
+youtube_url_entry = tk.Entry(youtube_entrys_frame, font=('Calibri', '16'))
+youtube_url_entry.insert(0, 'Youtube URL')
 youtube_url_entry.grid(column=0, row=0, padx=15, pady=12, sticky=tk.W)
 
-youtube_url_check_button = tk.Button(youtube_entrys_frame, text='Check', font=('Calibri', '12'), borderwidth=0, command= lambda: youtube.get_streams('https://www.youtube.com/watch?v=rFUXfIO_bNk'))
+youtube_url_check_button = tk.Button(youtube_entrys_frame, height=1, text='Check', font=('Calibri'), borderwidth=0, command= lambda: gui_command.check_button(youtube_url_entry.get(), selectable_stream))
 youtube_url_check_button.grid(row=0, column=1)
+
+selectable_stream = ttk.Combobox(root, font=('Calibri', '17'), width=23, state='readonly')
+selectable_stream.grid(column=0, row=1, padx=(25,0), pady=0)
+
+download_location_frame = tk.Frame(root, background='gray')
+download_location_frame.grid(column=0, row=2)
+
+download_location_entry = tk.Entry(download_location_frame, font=('Calibri', '16'), state='disabled')
+download_location_entry.grid(column=0, row=0, padx=(25,15), pady=12)
+
+download_location_button = tk.Button(download_location_frame, height=1, text='Browse', font=('Calibri'), borderwidth=0, command= lambda: gui_command.locate_file(download_location_entry))
+download_location_button.grid(row=0, column=1)
+
+download_button = tk.Button(root, height=1, width=30 ,text='DOWNLOAD', font=('Calibri'), borderwidth=0, command=lambda: gui_command.download(youtube_url_entry, selectable_stream, download_location_entry))
+download_button.grid(row=3, column=0, sticky=tk.W, padx=27)
 
 root.mainloop()
